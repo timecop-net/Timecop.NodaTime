5a9b7aa6000253060640a63c9b87653c698e41b8
Dmytro Khmara <dmytro@dmytrokhmara.com>
2023-07-31 22:16:33 +0100

    feat: add an instance clock. (#7)

---
+++ src/Timecop.NodaTime/NodaTimecopClock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NodaTime;
+
+namespace TCop.NodaTime;
+
+public class NodaTimecopClock: IClock
+{
+    private readonly Func<Instant> _getInstant;
+
+    public NodaTimecopClock(Func<Instant> getInstant)
+    {
+        _getInstant = getInstant;
+    }
+
+    public Instant GetCurrentInstant() => _getInstant();
+}
---
+++ test/Timecop.NodaTime.Tests/IClockTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using FluentAssertions.NodaTime;
+using NodaTime;
+using TCop.NodaTime;
+
+namespace TCop.Tests
+{
+    public class IClockTests
+    {
+        private readonly IClock _systemClock = SystemClock.Instance;
+        private static readonly Duration InstantComparisonPrecision = Duration.FromMilliseconds(50);
+
+        [Fact]
+        public void Clock_TimecopWithTravel_ShouldReturnCurrentTime()
+        {
+            var fiveHours = Duration.FromHours(5);
+
+            using var tc = new NodaTimecop();
+            tc.TravelBy(fiveHours);
+
+            tc.Clock.GetCurrentInstant().Should().BeCloseTo(_systemClock.GetCurrentInstant().Plus(fiveHours), InstantComparisonPrecision);
+        }
+
+        [Fact]
+        public void Clock_TwoClocksShouldBeTheSameInstance()
+        {
+            using var tc = new NodaTimecop();
+
+            tc.Clock.Should().Be(tc.Clock);
+        }
+    }
+}
